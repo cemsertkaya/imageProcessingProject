149b9797a94c8b5082837ce5c928f96916cf42ad
egecanceylan <egecanceylan@github.com>
2021-11-24 00:45:27 +0300

    histogram code clean

--- Histogram.py
+++ Histogram.py
@@ -1,26 +1,23 @@
 # tkinter and ttk module
 from tkinter import *
 from tkinter.ttk import *
-from tkinter import Label, Tk
+from tkinter import Label
 from PIL import Image, ImageTk
-from skimage import filters, color, img_as_float, io
-from skimage.filters import gabor
+from skimage import color
 import tkinter.filedialog as fd
 import numpy as np
-from skimage import data, exposure, img_as_float
-from matplotlib import pyplot as plt # import pylot
-from skimage.util import img_as_ubyte
-from skimage import data
-from skimage.exposure import histogram
+from matplotlib import pyplot as plt
 from skimage.filters import threshold_otsu
+
 
 class Histogram(Toplevel):
 
     def __init__(self, master=None):
         super().__init__(master=master)
         self.selectedImage = None
+        self.imageArray = None
         self.title("Histogram")
-        self.geometry("1000x1000")
+        self.geometry("1280x720")
         self.inputImageFrame = Frame(self)
         self.inputImageFrame.pack(side=LEFT)
         openImage = Button(self.inputImageFrame, text="Select Image", command=self.buttonClick)
@@ -35,57 +32,40 @@ class Histogram(Toplevel):
         invisText.pack(pady=100, padx=200)
 
     def createFilter(self, text):
-        filter = Button(self.buttonFrame, text=text, command=lambda: self.showChartClick(text))
+        filter = Button(self.buttonFrame, text=text, command=self.showChartClick)
         filter.pack(pady=20)
 
     def buttonClick(self):
         path = fd.askopenfilename(parent=self, filetypes=[("Image File", '.jpg'), ("Image File", '.png')])
-        self.selectedImage = Image.open(path)
+        self.selectedImage = Image.open(path).resize((400, 400))
         self.imageArray = np.array(self.selectedImage)
         tkImage = ImageTk.PhotoImage(self.selectedImage)
         myvar = Label(self.inputImageFrame, image=tkImage)
         myvar.pack()
         self.mainloop()
 
-    def showChartClick(self, text):
-        image = color.rgb2gray(self.imageArray)
-        thresh = threshold_otsu(image)
-        binary = image > thresh
+    def showChartClick(self):
+        if self.selectedImage is not None:
+            image = color.rgb2gray(self.imageArray)
+            thresh = threshold_otsu(image)
+            binary = image > thresh
 
-        fig, axes = plt.subplots(ncols=3, figsize=(8, 2.5))
-        ax = axes.ravel()
-        ax[0] = plt.subplot(1, 3, 1)
-        ax[1] = plt.subplot(1, 3, 2)
-        ax[2] = plt.subplot(1, 3, 3, sharex=ax[0], sharey=ax[0])
+            fig, axes = plt.subplots(ncols=3, figsize=(8, 2.5))
+            ax = axes.ravel()
+            ax[0] = plt.subplot(1, 3, 1)
+            ax[1] = plt.subplot(1, 3, 2)
+            ax[2] = plt.subplot(1, 3, 3, sharex=ax[0], sharey=ax[0])
 
-        ax[0].imshow(image, cmap=plt.cm.gray)
-        ax[0].set_title('Original')
-        ax[0].axis('off')
+            ax[0].imshow(image, cmap=plt.cm.gray)
+            ax[0].set_title('Original')
+            ax[0].axis('off')
 
-        ax[1].hist(image.ravel(), bins=256)
-        ax[1].set_title('Histogram')
-        ax[1].axvline(thresh, color='r')
+            ax[1].hist(image.ravel(), bins=256)
+            ax[1].set_title('Histogram')
+            ax[1].axvline(thresh, color='r')
 
-        ax[2].imshow(binary, cmap=plt.cm.gray)
-        ax[2].set_title('Thresholded')
-        ax[2].axis('off')
+            ax[2].imshow(binary, cmap=plt.cm.gray)
+            ax[2].set_title('Thresholded')
+            ax[2].axis('off')
 
-        plt.show()
-        '''
-        noisy_image = img_as_ubyte(self.imageArray)
-
-        hist, hist_centers = histogram(color.rgb2gray(noisy_image))
-
-        fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
-
-        ax[0].imshow(noisy_image, cmap=plt.cm.gray)
-        ax[0].axis('off')
-
-        ax[1].plot(hist_centers, hist, lw=2)
-        ax[1].set_title('Gray-level histogram')
-
-        plt.tight_layout()
-        plt.show()
-        '''
-        #Grafik Gösterildi Eşikleme Yapılması Lazım
-
+            plt.show()
